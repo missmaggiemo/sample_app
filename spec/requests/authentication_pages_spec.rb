799dd9ea545ec8eaf77e155acf46af2fb7c20e3d
missmaggiemo <margaret.m.moreno@gmail.com>
2014-01-02 21:39:37 -0800

    Rspec custom matchers

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,7 +14,7 @@ describe "Authentication" do
       before {click_button "Sign in"}
       
       it {should have_title("Sign In")}
-      it {should have_selector('div.alert.alert-error')}
+      it {should have_error_message("Invalid")}
       
       describe "after visiting another page" do
         before {click_link "Home"}
@@ -24,15 +24,12 @@ describe "Authentication" do
     
     describe "with valid info" do
       let(:user) {FactoryGirl.create(:user)}
-      before do
-        fill_in "Email", with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before {valid_signin(user)}
       
       it {should have_title(user.name)}
       it {should have_link('Profile', href: user_path(user))}
       it {should have_link('Sign out', href: signout_path)}
+      it {should_not have_link("Sign in", href: signin_path)}
       
       describe "followed by signout" do
         before {click_link "Sign out"}
@@ -42,7 +39,6 @@ describe "Authentication" do
     end
   end
   
-
 end
 
 
